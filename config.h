fef6d3e1e69abeae8f437ecba3477af0b824bc5c
Viktar Palstsiuk <viktar.palstsiuk@promwad.com>
2019-12-25 16:16:07 +0300

    Added SENSOR_MAC_ADDR

--- config.h
+++ config.h
@@ -1,7 +1,8 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define BEACON_MAC_ADDR "01:02:03:04:05:06"
+#define BEACON_MAC_ADDR "DF:3C:C4:72:99:DE"
+#define SENSOR_MAC_ADDR "4C:65:A8:DD:89:9B"
 
 #define IO_USERNAME  "username"
 #define IO_KEY       "0102030405060708090a0b0c0d0e0f"
